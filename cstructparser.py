7ab0b5538057be13085c85eac370a448491885db
Matthias Tamegger <tamegger.matthias@gmail.com>
2021-09-30 21:40:38 +0200

    Fix import variable names

--- cstructparser.py
+++ cstructparser.py
@@ -1,8 +1,8 @@
 import enum
 import re
 from dataclasses import dataclass
-from dataclasses import field
-from typing import IO, Optional
+from dataclasses import field as field_parameter
+from typing import Optional
 
 from lineprovider import LineProvider
 
@@ -26,9 +26,9 @@ class Struct:
 class _StructInit:
     name: str
     hasTypedef: bool
-    fields: list[Field] = field(default_factory=lambda: [])
+    fields: list[Field] = field_parameter(default_factory=lambda: [])
     innerStructsInit = None
-    innerStructs: list[Struct] = field(default_factory=lambda: [])
+    innerStructs: list[Struct] = field_parameter(default_factory=lambda: [])
 
 
 class _ParserState(enum.Enum):
@@ -46,100 +46,101 @@ C_STRUCT_FIELD_MATCHER = f"({C_SYMBOL_MATCHER}) (\*)?({C_SYMBOL_MATCHER})(?:\[([
 class HeaderFileParser:
 
     @staticmethod
-    def __matchStructStart(line):
+    def __match_struct_start(line):
         return re.match(C_STRUCT_START_MATCHER, line)
 
     @staticmethod
-    def __matchStructEnd(line):
+    def __match_struct_end(line):
         return re.match(C_STRUCT_END_MATCHER, line)
 
     @staticmethod
-    def __searchField(line):
+    def __search_field(line):
         return re.search(C_STRUCT_FIELD_MATCHER, line)
 
     @staticmethod
-    def __readStructInit(line) -> Optional[_StructInit]:
-        line_match = HeaderFileParser.__matchStructStart(line)
+    def __read_struct_init(line) -> Optional[_StructInit]:
+        line_match = HeaderFileParser.__match_struct_start(line)
         if not line_match:
             return None
 
         return _StructInit(line_match.group(2), line_match.group(1) is not None)
 
     @staticmethod
-    def __isEmptyLine(line):
+    def __is_empty_line(line):
         return re.match("^(?!\s*$).+", line) is None
 
     @staticmethod
-    def __readField(line) -> Optional[Field]:
-        line_match = HeaderFileParser.__searchField(line)
+    def __read_field(line) -> Optional[Field]:
+        line_match = HeaderFileParser.__search_field(line)
         if not line_match:
             return None
         size = int(line_match.group(4)) if line_match.group(4) else 1
         return Field(line_match.group(3), line_match.group(1), line_match.group(2) is not None, size)
 
     @staticmethod
-    def __handleReadStructContent(line: str, structInit: _StructInit) -> Optional[Struct]:
-        if structInit.innerStructsInit:
-            struct = HeaderFileParser.__handleReadStructContent(line, structInit.innerStructsInit)
+    def __handle_read_struct_content(line: str, struct_init: _StructInit) -> Optional[Struct]:
+        if struct_init.innerStructsInit:
+            struct = HeaderFileParser.__handle_read_struct_content(line, struct_init.innerStructsInit)
             if not struct:
                 return None
-            structInit.innerStructsInit = None
-            structInit.innerStructs.append(struct)
+            struct_init.innerStructsInit = None
+            struct_init.innerStructs.append(struct)
             assert struct.name is not None
             field = Field(struct.name, struct.name, False, 1)
-            structInit.fields.append(field)
+            struct_init.fields.append(field)
             return None
 
-        innerStructInit = HeaderFileParser.__readStructInit(line)
-        if innerStructInit:
-            structInit.innerStructsInit = innerStructInit
+        inner_struct_init = HeaderFileParser.__read_struct_init(line)
+        if inner_struct_init:
+            struct_init.innerStructsInit = inner_struct_init
             return None
 
-        field = HeaderFileParser.__readField(line)
+        field = HeaderFileParser.__read_field(line)
         if field:
-            structInit.fields.append(field)
+            struct_init.fields.append(field)
             return None
 
-        if HeaderFileParser.__isEmptyLine(line):
+        if HeaderFileParser.__is_empty_line(line):
             return None
 
-        structEnd = HeaderFileParser.__matchStructEnd(line)
-        if structEnd:
-            structTypedef = structEnd.group(1)
-            structName = structInit.name if structInit.hasTypedef or not structTypedef else structTypedef
+        struct_end = HeaderFileParser.__match_struct_end(line)
+        if struct_end:
+            struct_typedef = struct_end.group(1)
+            struct_name = struct_init.name if struct_init.hasTypedef or not struct_typedef else struct_typedef
 
             return Struct(
-                structName,
-                structTypedef,
-                structInit.fields
+                struct_name,
+                struct_typedef,
+                struct_init.fields
             )
 
         return None
 
-    def parse(self, lineProvider: LineProvider):
-        lineProvider = CommentRemovingLineProvider(lineProvider)
+    @staticmethod
+    def parse(line_provider: LineProvider):
+        line_provider = CommentRemovingLineProvider(line_provider)
         parser_state = _ParserState.FindStructStart
         struct_init: _StructInit
         structs = []
 
         while True:
-            lineProvider.next()
+            line_provider.next()
 
-            if not lineProvider.line():
+            if not line_provider.line():
                 break
 
             if parser_state == _ParserState.FindStructStart:
-                struct_init = HeaderFileParser.__readStructInit(lineProvider.line())
+                struct_init = HeaderFileParser.__read_struct_init(line_provider.line())
                 if struct_init:
                     parser_state = _ParserState.ReadStructContent
                     print(f"Found struct with name {struct_init.name}, read content now")
             elif parser_state == _ParserState.ReadStructContent:
-                struct = HeaderFileParser.__handleReadStructContent(lineProvider.line(), struct_init)
+                struct = HeaderFileParser.__handle_read_struct_content(line_provider.line(), struct_init)
                 if struct:
                     structs.append(struct)
                     parser_state = _ParserState.FindStructStart
             elif parser_state == _ParserState.FindEnd:
-                line_match = re.match(C_STRUCT_END_MATCHER, lineProvider.line())
+                line_match = re.match(C_STRUCT_END_MATCHER, line_provider.line())
                 if not line_match:
                     continue
                 parser_state = _ParserState.FindStructStart
@@ -154,8 +155,8 @@ class CommentRemovingLineProvider(LineProvider):
     __currentLine: str = None
     __multilineCommendStarted: bool = False
 
-    def __init__(self, lineProvider):
-        self.__lineProvider = lineProvider
+    def __init__(self, line_provider):
+        self.__lineProvider = line_provider
 
     def __preprocess_line(self, line):
         if self.__multilineCommendStarted:
