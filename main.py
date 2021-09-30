92e471d79ebe29656f47a5cc129206ffa2c153c3
Matthias Tamegger <matamegger@gmail.com>
2021-09-30 21:40:38 +0200

    Fix import variable names

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import os.path
 import sys
-from HeaderFileParser import HeaderFileParser, CommentRemovingLineProvider, FileLineProvider
+from cstructparser import HeaderFileParser
+from lineprovider import FileLineProvider
 
 if __name__ == '__main__':
-    mainFilePath = os.path.expanduser(sys.argv[1])
-    mainFile = open(mainFilePath, "r")
+    main_file_path = os.path.expanduser(sys.argv[1])
+    main_file = open(main_file_path, "r")
 
     parser = HeaderFileParser()
 
-    structs = parser.parse(FileLineProvider(mainFile))
+    structs = parser.parse(FileLineProvider(main_file))
 
     for struct in structs:
         print(struct)
