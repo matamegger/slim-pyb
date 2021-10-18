98bdec478d94b021348ece732edce272b94c8b9c
Matthias Tamegger <matamegger@gmail.com>
2021-10-18 19:31:25 +0200

    Split up function into a base function finding the type and one that just converts to a string then

--- astparser/__init__.py
+++ astparser/__init__.py
@@ -1,14 +1,23 @@
-from astparser.types import Type, NamedType, Pointer, StructType, Array
+from astparser.types import Type, NamedType, Pointer, StructType, Array, FunctionType
+
+
+def get_base_type(typ: Type) -> Type:
+    if isinstance(typ, NamedType):
+        return typ
+    elif isinstance(typ, Pointer):
+        return get_base_type(typ.of)
+    elif isinstance(typ, Array):
+        return get_base_type(typ.of)
+    elif isinstance(typ, StructType):
+        return typ
+    elif isinstance(typ, FunctionType):
+        return typ
+    else:
+        raise Exception(f"Unhandled case {typ}")
 
 
 def get_base_type_name(typ: Type) -> str:
-    if isinstance(typ, NamedType):
-        return typ.name
-    elif isinstance(typ, Pointer):
-        return get_base_type_name(typ)
-    elif isinstance(typ, StructType):
-        return typ.name
-    elif isinstance(typ, Array):
-        return get_base_type_name(typ.of)
-    else:
-        raise Exception("Unhandled case")
+    base_type = get_base_type(typ)
+    if isinstance(base_type, NamedType) or isinstance(base_type, StructType):
+        return base_type.name
+    raise Exception("Can not get base type name")
