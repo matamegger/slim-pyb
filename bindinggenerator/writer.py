bab9ffddb8f57c983779a9556a11a11f1b9db71c
Matthias Tamegger <matamegger@gmail.com>
2021-10-21 16:11:38 +0200

    Move around globals

--- bindinggenerator/writer.py
+++ bindinggenerator/writer.py
@@ -2,6 +2,7 @@ import ctypes
 from typing import IO
 
 from astparser import get_base_type_name
+from bindinggenerator import primitive_names_to_ctypes
 from bindinggenerator.model import File, Import, Element, Definition, Enum, CtypeStruct, CtypeStructDefinition, \
     CtypeStructDeclaration, CtypeFieldPointer, CtypeFieldType, NamedCtypeFieldType, CtypeFieldTypeArray, \
     CtypeFieldFunctionPointer
@@ -16,40 +17,6 @@ class Output:
 
     def close(self):
         pass
-
-primitive_names_to_ctypes = {
-    "byte": ctypes.c_byte,
-    "char": ctypes.c_char,
-    "signed char": ctypes.c_char,
-    "short": ctypes.c_short,
-    "int": ctypes.c_int,
-    "unsigned": ctypes.c_int,
-    "int8": ctypes.c_int8,
-    "int16": ctypes.c_int16,
-    "int32": ctypes.c_int32,
-    "int64": ctypes.c_int64,
-    "long": ctypes.c_long,
-    "long int": ctypes.c_long,
-    "long long": ctypes.c_longlong,
-    "float": ctypes.c_float,
-    "double": ctypes.c_double,
-    "long double": ctypes.c_longdouble,
-    "unsigned byte": ctypes.c_ubyte,
-    "unsigned char": ctypes.c_ubyte,
-    "unsigned short": ctypes.c_ushort,
-    "unsigned int": ctypes.c_uint,
-    "unsigned int8": ctypes.c_uint8,
-    "unsigned int16": ctypes.c_uint16,
-    "unsigned int32": ctypes.c_uint32,
-    "unsigned int64": ctypes.c_uint64,
-    "unsigned long": ctypes.c_ulong,
-    "unsigned long long": ctypes.c_ulonglong,
-    "size_t": ctypes.c_size_t,
-    "ptrdiff_t": ctypes.c_ssize_t,
-    "void": None
-}
-
-primitive_names = set([k for k, v in primitive_names_to_ctypes.items()])
 
 
 def _ctype_to_string(ctype) -> str:
