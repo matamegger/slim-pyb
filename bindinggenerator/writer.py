b2bdfb42f84ff380f257589a09a8842c5708cfe8
Matthias Tamegger <tamegger.matthias@gmail.com>
2021-11-19 22:27:48 +0100

    Handle Union in binding writer

--- bindinggenerator/writer.py
+++ bindinggenerator/writer.py
@@ -1,11 +1,11 @@
 import ctypes
 import os.path
-from typing import IO
+from typing import IO, Union
 
 from bindinggenerator import primitive_names_to_ctypes
 from bindinggenerator.model import BindingFile, Import, Element, Definition, Enum, CtypeStruct, CtypeStructDefinition, \
     CtypeStructDeclaration, CtypeFieldPointer, CtypeFieldType, NamedCtypeFieldType, CtypeFieldTypeArray, \
-    CtypeFieldFunctionPointer, CtypeStructField, System, SystemMethod, SystemField
+    CtypeFieldFunctionPointer, CtypeStructField, System, SystemMethod, SystemField, CtypeUnion, CtypeUnionDeclaration
 
 
 class Output:
@@ -157,11 +157,12 @@ class PythonBindingWriter(BaseWriter):
     __ENUM_DECLARATION_PATTER = "class {0}(Enum):"
     __ENUM_ENTRY_PATTERN = "{0} = {1}"
     __STRUCT_DECLARATION_PATTERN = "class {0}(ctypes.Structure):"
-    __STRUCT_FIELD_ASSIGNMENT_START = "_fields_ = ["
-    __STRUCT_FIELD_OR_SLOTS_ASSIGNMENT_END = "]"
-    __STRUCT_FIELD_PATTERN = "('{0}', {1})"
-    __STRUCT_SLOTS_ASSIGNMENT_START = "__slots__ = ["
-    __STRUCT_SLOT_PATTERN = "'{0}'"
+    __FIELD_ASSIGNMENT_START = "_fields_ = ["
+    __FIELD_OR_SLOTS_ASSIGNMENT_END = "]"
+    __FIELD_PATTERN = "('{0}', {1})"
+    __SLOTS_ASSIGNMENT_START = "__slots__ = ["
+    __SLOT_PATTERN = "'{0}'"
+    __UNION_DECLARATION_PATTERN = "class {0}(ctypes.Union):"
 
     def write(self, file: BindingFile, output: Output):
         for imprt in file.imports:
@@ -185,26 +186,38 @@ class PythonBindingWriter(BaseWriter):
             output.write(self.__STRUCT_DECLARATION_PATTERN.format(element.name))
             output.new_line()
             output.new_line()
-            self.__write_struct_declaration(element, False, output, 1)
+            self.__write_struct_or_union_declaration(element, False, output, 1)
         elif isinstance(element, CtypeStructDefinition):
             output.write(self.__STRUCT_DECLARATION_PATTERN.format(element.name))
             output.new_line()
             output.write(self._INDENT)
             output.write(self.__PASS)
         elif isinstance(element, CtypeStructDeclaration):
-            self.__write_struct_declaration(element, True, output, 0)
+            self.__write_struct_or_union_declaration(element, True, output, 0)
+        elif isinstance(element, CtypeUnion):
+            output.write(self.__UNION_DECLARATION_PATTERN.format(element.name))
+            output.new_line()
+            output.new_line()
+            self.__write_struct_or_union_declaration(element, False, output, 1)
+        elif isinstance(element, CtypeStructDefinition):
+            output.write(self.__UNION_DECLARATION_PATTERN.format(element.name))
+            output.new_line()
+            output.write(self._INDENT)
+            output.write(self.__PASS)
+        elif isinstance(element, CtypeUnionDeclaration):
+            self.__write_struct_or_union_declaration(element, True, output, 0)
         else:
             raise Exception(f"Unhandled element {element}")
 
-    def __write_struct_declaration(self, declaration: CtypeStructDeclaration, with_class_name: bool, output: Output,
-                                   indent: int):
-        self.__write_struct_declaration_slots(declaration, with_class_name, output, indent)
+    def __write_struct_or_union_declaration(self, declaration: Union[CtypeStructDeclaration, CtypeUnionDeclaration], with_class_name: bool, output: Output,
+                                            indent: int):
+        self.__write_struct_or_union_declaration_slots(declaration, with_class_name, output, indent)
         output.new_line()
         self.__write_struct_declaration_fields(declaration, with_class_name, output, indent)
 
-    def __write_struct_declaration_slots(
+    def __write_struct_or_union_declaration_slots(
             self,
-            declaration: CtypeStructDeclaration,
+            declaration: Union[CtypeStructDeclaration, CtypeUnionDeclaration],
             with_class_name: bool,
             output: Output, indent: int
     ):
@@ -212,7 +225,7 @@ class PythonBindingWriter(BaseWriter):
         if with_class_name:
             output.write(declaration.name)
             output.write(".")
-        output.write(self.__STRUCT_SLOTS_ASSIGNMENT_START)
+        output.write(self.__SLOTS_ASSIGNMENT_START)
         output.new_line()
         indent = indent + 1
         for field in declaration.fields[:-1]:
@@ -223,12 +236,12 @@ class PythonBindingWriter(BaseWriter):
         output.new_line()
         indent -= 1
         self._write_indent(output, indent)
-        output.write(self.__STRUCT_FIELD_OR_SLOTS_ASSIGNMENT_END)
+        output.write(self.__FIELD_OR_SLOTS_ASSIGNMENT_END)
         output.new_line()
 
     def __write_struct_declaration_fields(
             self,
-            declaration: CtypeStructDeclaration,
+            declaration: Union[CtypeStructDeclaration, CtypeUnionDeclaration],
             with_class_name: bool,
             output: Output, indent: int
     ):
@@ -236,7 +249,7 @@ class PythonBindingWriter(BaseWriter):
         if with_class_name:
             output.write(declaration.name)
             output.write(".")
-        output.write(self.__STRUCT_FIELD_ASSIGNMENT_START)
+        output.write(self.__FIELD_ASSIGNMENT_START)
         output.new_line()
         indent = indent + 1
         for field in declaration.fields[:-1]:
@@ -247,16 +260,16 @@ class PythonBindingWriter(BaseWriter):
         output.new_line()
         indent -= 1
         self._write_indent(output, indent)
-        output.write(self.__STRUCT_FIELD_OR_SLOTS_ASSIGNMENT_END)
+        output.write(self.__FIELD_OR_SLOTS_ASSIGNMENT_END)
         output.new_line()
 
     def __write_struct_declaration_slot(self, field: CtypeStructField, output: Output, indent: int):
         self._write_indent(output, indent)
-        output.write(self.__STRUCT_SLOT_PATTERN.format(field.name))
+        output.write(self.__SLOT_PATTERN.format(field.name))
 
     def __write_struct_declaration_field(self, field: CtypeStructField, output: Output, indent: int):
         self._write_indent(output, indent)
-        output.write(self.__STRUCT_FIELD_PATTERN.format(field.name, self.__mapping(field.type)))
+        output.write(self.__FIELD_PATTERN.format(field.name, self.__mapping(field.type)))
 
     def __mapping(self, typ: CtypeFieldType) -> str:
         return self._mapper.get_mapping(typ)
